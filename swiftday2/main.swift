dd973da918bd9f0193215aa4f20ff2549f2beadb
MacStudent <macstudent@126-imac22.local>
2019-02-08 19:11:02 -0500

    Types of Range listed

--- swiftday2/main.swift
+++ swiftday2/main.swift
@@ -17,3 +17,62 @@ print("Hello, World!")
  }
  */
 
+var s:String="Hello"
+print(s)
+var str=String()
+str="Hello World"
+print(str)
+var p:String
+p="Call me"
+print (p,str,s)
+
+print(s.count)
+print(str.description)
+print(str.hasPrefix("Hello"))
+print(str.hasPrefix("Hello"))
+print(str.hasSuffix("me"))
+print(str.hasPrefix("me"))
+print(str.lowercased())
+print(str.uppercased())
+
+//Display Character for String
+for c in str
+{
+    print(c)
+}
+
+var x = 5...10
+
+for i in x
+{
+    print(i)
+    
+}
+print(x)
+
+var y = 1...
+for i in y
+{
+  print(i)
+    if(i==20)
+    {
+        break
+        
+    }
+}
+
+var z = ...50
+
+if( z.contains(-1000))
+{
+    print("True")
+}
+else{
+    print("false")
+}
+
+var a=[10,20,30,40,50]
+for i in a
+{
+    print(i)
+}
